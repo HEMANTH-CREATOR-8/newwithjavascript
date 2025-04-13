c79e31eacad68fc7cabd69178b8beb93dc0e4d18
HEMANTH-CREATOR-8 <mhr122007@gmail.com>
2025-04-13 22:28:31 +0530

    Update siri.js

--- siri.js
+++ siri.js
@@ -1,8 +1,13 @@
 function myfun(){
   let username = document.getElementById("entername").value;
   let password = document.getElementById("password").value;
-  if (username === "hemanth" && password === "nani") {
-   window.location.href="SUBMIT.HTML";
+  if (username === "hemanth") {
+    if (password ==="nani"){
+      window.location.href="SUBMIT.HTML";
+    }
+    else{
+      window.alert("incorrect password");
+    }
   } 
   else {
     window.alert("enter valid username and password");
